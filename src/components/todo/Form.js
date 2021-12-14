c8cb42d099039a5ee796f60f7730d573943f3ae5
marwanrawshedh <marwansalem55@yahoo.com>
2021-12-14 19:56:12 +0200

    lab 33

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -1,15 +1,19 @@
-import React, { useContext, useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ContextHandle } from "./Context";
+import { When } from 'react-if';
 import { Button, Card, Elevation } from "@blueprintjs/core";
 import "./header.css";
-
+import { parse } from "uuid";
+import { LoginContext } from "../auth/context";
 function Form(props) {
+  const data1 = useContext(LoginContext);
+ 
   const data = useContext(ContextHandle);
-  const get=localStorage.getItem("num");
-  
+  const get = parseInt(localStorage.getItem("num"));
+
   const [pageNumber, setPageNumber] = useState({
     start: 0,
-    end:  get,
+    end: get,
     numPages: 1,
     cure: 1,
   });
@@ -41,92 +45,114 @@ function Form(props) {
   }
   return (
     <>
-      <Card className="form" interactive={true} elevation={Elevation.FOUR}>
-        <form onSubmit={data.handleSubmit}>
-          <h2>Add To Do Item</h2>
+      {data1.loggedIn && 
+        <>
+          <Card className="form" interactive={true} elevation={Elevation.FOUR}>
+            <form onSubmit={data.handleSubmit}>
+              <h2>Add To Do Item</h2>
 
-          <label>
-            <span>To Do Item</span>
-            <input
-              onChange={data.handleChange}
-              name="text"
-              type="text"
-              placeholder="Item Details"
-            />
-          </label>
+              <label>
+                <span>To Do Item</span>
+                <input
+                  onChange={data.handleChange}
+                  name="text"
+                  type="text"
+                  placeholder="Item Details"
+                />
+              </label>
 
-          <label>
-            <span>Assigned To</span>
-            <input
-              onChange={data.handleChange}
-              name="assignee"
-              type="text"
-              placeholder="Assignee Name"
-            />
-          </label>
+              <label>
+                <span>Assigned To</span>
+                <input
+                  onChange={data.handleChange}
+                  name="assignee"
+                  type="text"
+                  placeholder="Assignee Name"
+                />
+              </label>
 
-          <label>
-            <span>Difficulty</span>
-            <input
-              onChange={data.handleChange}
-              defaultValue={3}
-              type="range"
-              min={1}
-              max={5}
-              name="difficulty"
-            />
-          </label>
+              <label>
+                <span>Difficulty</span>
+                <input
+                  onChange={data.handleChange}
+                  defaultValue={3}
+                  type="range"
+                  min={1}
+                  max={5}
+                  name="difficulty"
+                />
+              </label>
+              <When condition={data1.can("create")}>
 
-          <label>
-            <button type="submit">Add Item</button>
-          </label>
-        </form>
-{/* <button onClick={()=>{data.setChangecomplete(data.changecomplete?false:true)}}> {`${data.changecomplete}`}</button> */}
-        <div>
-          <button onClick={() => handelPrev()}>prev</button>
-          <label>
-            {pageNumber.cure}/{pageNumber.numPages}
-          </label>
-          <button onClick={() => handelNext()}>next</button>
-        </div>
-      </Card>
-    
-      {data.isTrue&&data.list.slice(pageNumber.start, pageNumber.end).map((item) => (
-        <div className="card" key={item.id}>
-          <Card interactive={true} elevation={Elevation.FOUR}>
-            <p>{item.text}</p>
-            <p>
-              <small>Assigned to: {item.assignee}</small>
-            </p>
-            <p>
-              <small>Difficulty: {item.difficulty}</small>
-            </p>
-            <div>Complete: {item.complete.toString()}</div>
-            <Button onClick={() => data.toggleComplete(item.id)}>
-              complete
-            </Button>
+              <label>
 
-          </Card>
-        </div>
-      ))}
-        {!data.isTrue&&data.list.filter(word => word.complete === false).slice(pageNumber.start, pageNumber.end).map((item) => (
-        <div className="card" key={item.id}>
-          <Card interactive={true} elevation={Elevation.FOUR}>
-            <p>{item.text}</p>
-            <p>
-              <small>Assigned to: {item.assignee}</small>
-            </p>
-            <p>
-              <small>Difficulty: {item.difficulty}</small>
-            </p>
-            <div>Complete: {item.complete.toString()}</div>
-            <Button onClick={() => data.toggleComplete(item.id)}>
-              complete
-            </Button>
+                <button type="submit">Add Item</button>
+              </label>
+              </When >
+            </form>
             
+            <div>
+              <button onClick={() => handelPrev()}>prev</button>
+              <label>
+                {pageNumber.cure}/{pageNumber.numPages}
+              </label>
+              <button onClick={() => handelNext()}>next</button>
+            </div>
           </Card>
-        </div>
-      ))}
+
+          {data.isTrue &&
+            data.list.slice(pageNumber.start, pageNumber.end).map((item) => (
+              <div className="card" key={item.id}>
+                <Card interactive={true} elevation={Elevation.FOUR}>
+                  <p>{item.text}</p>
+                  <p>
+                    <small>Assigned to: {item.assignee}</small>
+                  </p>
+                  <p>
+                    <small>Difficulty: {item.difficulty}</small>
+                  </p>
+                  <div>Complete: {item.complete.toString()}</div>
+                  <Button onClick={() => data.toggleComplete(item.id)}>
+                    complete
+                  </Button>
+                  <Button onClick={() => data.deleteItem(item.id)}>
+                   delete
+                  </Button>
+                </Card>
+              </div>
+            ))}
+          {!data.isTrue &&
+            data.list
+              .filter((word) => word.complete === false)
+              .slice(pageNumber.start, pageNumber.end)
+              .map((item) => (
+                <div className="card" key={item.id}>
+                  <Card interactive={true} elevation={Elevation.FOUR}>
+                    <p>{item.text}</p>
+                    <p>
+                      <small>Assigned to: {item.assignee}</small>
+                    </p>
+                    <p>
+                      <small>Difficulty: {item.difficulty}</small>
+                    </p>
+                    <div>Complete: {item.complete.toString()}</div>
+                    <When condition={data1.can("update")}>
+
+                    <Button onClick={() => data.toggleComplete(item.id)}>
+                      complete
+
+                    </Button>
+                    </When>
+                      <When condition={data1.can("delete")}>
+                    <Button onClick={() => data.deleteItem(item.id)}>
+                   delete
+                  </Button>
+                      </When>
+                  </Card>
+                </div>
+              ))}
+        </>
+      }
     </>
   );
 }
--- src/components/todo/Header.js
+++ src/components/todo/Header.js
@@ -1,12 +1,16 @@
 import React,{useContext} from 'react';
-
+import { When } from 'react-if';
 import { Link } from 'react-router-dom';
 import "./header.css";
+import { LoginContext } from "../auth/context";
 function Header(props) {
-    
+  const data1 = useContext(LoginContext);
    
     return (
-        <header>
+      <header>
+          
+          {data1.loggedIn && 
+
           <div>
 
           <button >
@@ -15,7 +19,9 @@ function Header(props) {
           </button>
           <button><Link to="/setting"> setting</Link></button>
           </div>
-        
+}<When condition={data1.loggedIn}>
+          <button onClick={data1.logout}>Log Out</button>
+        </When>
       </header>
     );
 }
